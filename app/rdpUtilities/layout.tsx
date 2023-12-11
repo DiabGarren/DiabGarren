a4951c9a573acc5bf6168855c329eb2f02ba7fc2
diabGarren <garrendiab@gmail.com>
2023-12-11 16:54:47 +0200

    Added icon

--- app/rdpUtilities/layout.tsx
+++ app/rdpUtilities/layout.tsx
@@ -1,12 +1,12 @@
-import "./globals.css";
+import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Home",
-    description: "Home page",
+    title: "Login",
+    description: "Login to rdpUtilties",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
