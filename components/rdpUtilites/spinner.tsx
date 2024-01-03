55df3d937bec998b3860106803265892476bb1c6
diabGarren <garrendiab@gmail.com>
2024-01-04 00:15:19 +0200

    Added loading spinner

---
+++ components/rdpUtilites/spinner.tsx
@@ -0,0 +1,6 @@
+export default function Spinner() {
+    return (
+        <div
+            className={`bg-[url('/rdputilities/spinner.svg')] w-[50px] h-[50px] animate-spin-slow`}></div>
+    );
+}
