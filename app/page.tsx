3faf752f3064070cd47b2a397639d940d385e85f
diabGarren <garrendiab@gmail.com>
2024-01-09 16:39:53 +0200

    Start to 3d print site

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 export default function Home() {
     return (
-        <main>
+        <main className="w-[350px] mx-auto">
             <a
-                className="button"
+                className="button bg-blue hover:text-blue text-center"
                 href="/rdpUtilities">
                 RdpUtilities
+            </a>
+            <a
+                className="button bg-blue hover:text-blue text-center"
+                href="/3DPrinting">
+                3D Printing
             </a>
         </main>
     );
