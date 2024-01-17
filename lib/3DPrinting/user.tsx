6501e8058407f1b1896b4a2f3544c7b6909484f1
diabGarren <garrendiab@gmail.com>
2024-01-17 18:51:54 +0200

    Added user interface

---
+++ lib/3DPrinting/user.tsx
@@ -0,0 +1,13 @@
+export interface User {
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    phone: string;
+    prefer: string;
+    password: string;
+    level: number;
+    cart: [
+        { _id: string; name: string; size: string; price: number; colour: string; image: string }
+    ];
+}
