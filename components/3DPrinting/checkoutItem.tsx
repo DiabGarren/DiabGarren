981f112358123fd96fe6e2f03ed53fc66765209e
diabGarren <garrendiab@gmail.com>
2024-01-20 01:04:48 +0200

    Minor style changes

--- components/3DPrinting/checkoutItem.tsx
+++ components/3DPrinting/checkoutItem.tsx
@@ -2,7 +2,7 @@ import ImageFallback from "./imageFallback";
 
 export default function CheckoutItem(props: any) {
     return (
-        <div className="flex border-b border-print-grey-light-1 py-[5px] last:border-none gap-[5px]">
+        <div className="flex border-b border-print-grey-light-1 py-[5px] last:border-none gap-[5px] justify-center">
             <div className="w-[30%] self-center">
                 <ImageFallback
                     src={props.image}
@@ -10,8 +10,7 @@ export default function CheckoutItem(props: any) {
                     width={200}
                 />
             </div>
-
-            <div>
+            <div className="w-[200px]">
                 <h3 className="text-[18px] font-[500]">{props.name}</h3>
                 <p>Size: {props.size}</p>
                 <p>Colour: {props.colour}</p>
