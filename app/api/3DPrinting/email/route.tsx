d1c9f2b5d7ac5dd38e10ce80ba6cf2d6f53dec32
diabGarren <garrendiab@gmail.com>
2024-01-24 00:54:01 +0200

    Removed send email

--- app/api/3DPrinting/email/route.tsx
+++ app/api/3DPrinting/email/route.tsx
@@ -4,26 +4,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
-        sgMail.setApiKey(`${process.env.SENDGRID_API}`);
-        const message = {
-            to: `${process.env.GMAIL}`,
-            from: `${process.env.GMAIL_SELF}`,
-            subject: "Welcome to Diab Desgin's 3D Printing!",
-            html: `<h2>Test email</h2><p>Thank you for creating an account.<br>Contact <a href="mailto:garrendiab@gmail.com">@Garren Diab</a> if you have any queries or concerns.</p>`,
-        };
-        try {
-            await sgMail.send(message);
-            console.log("Email sent");
-        } catch (error) {
-            console.log(error);
-        }
-
-        let response = { status: "success" };
-
-        return new NextResponse(JSON.stringify(response), {
-            status: 201,
-            headers: { "Content-Type": "application/json" },
-        });
     } catch (error: any) {
         return createErrorResponse(error.message, 500);
     }
