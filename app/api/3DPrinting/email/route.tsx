fc9445d864eb57e7380e9884ce5290ac9a27cb35
diabGarren <garrendiab@gmail.com>
2024-01-24 00:55:56 +0200

    Fixed email api

--- app/api/3DPrinting/email/route.tsx
+++ app/api/3DPrinting/email/route.tsx
@@ -4,6 +4,10 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
     try {
+        return new NextResponse(JSON.stringify({}), {
+            status: 201,
+            headers: { "Content-Type": "application/json" },
+        });
     } catch (error: any) {
         return createErrorResponse(error.message, 500);
     }
