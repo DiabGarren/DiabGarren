ef286d2f7a3125313625e47fce885da5a74ff082
DiabGarren <garrendiab@gmail.com>
2024-06-01 19:00:18 +0200

    Start to calendar

--- app/page.tsx
+++ app/page.tsx
@@ -22,75 +22,12 @@ export default function Home() {
             >
                 3D Printing
             </a>
-            <div className="container">
-                <Button
-                    className="col-[1/4]"
-                    onClick={() => {
-                        if (xPlane.includes(face) && face != "back")
-                            setFace("top");
-                        else if (yPlane.indexOf(face) != yPlane.length - 1)
-                            setFace(yPlane[yPlane.indexOf(face) + 1]);
-                        else setFace(yPlane[0]);
-                    }}
-                >
-                    ^
-                </Button>
-
-                <Button
-                    onClick={() => {
-                        if (
-                            yPlane.includes(face) &&
-                            face != "front" &&
-                            face != "back"
-                        )
-                            setFace("left");
-                        else if (xPlane.indexOf(face) != 0)
-                            setFace(xPlane[xPlane.indexOf(face) - 1]);
-                        else setFace(xPlane[xPlane.length - 1]);
-                    }}
-                >
-                    {"<"}
-                </Button>
-
-                <div className="scene">
-                    <div className={`cube show-${face}`}>
-                        <div id="front" className="cube-face "></div>
-                        <div id="right" className="cube-face "></div>
-                        <div id="back" className="cube-face "></div>
-                        <div id="left" className="cube-face "></div>
-                        <div id="top" className="cube-face bg-white"></div>
-                        <div id="bottom" className="cube-face bg-white"></div>
-                    </div>
-                </div>
-
-                <Button
-                    onClick={() => {
-                        if (
-                            yPlane.includes(face) &&
-                            face != "front" &&
-                            face != "back"
-                        )
-                            setFace("right");
-                        else if (xPlane.indexOf(face) != xPlane.length - 1)
-                            setFace(xPlane[xPlane.indexOf(face) + 1]);
-                        else setFace(xPlane[0]);
-                    }}
-                >
-                    {">"}
-                </Button>
-                <Button
-                    className="col-[1/4]"
-                    onClick={() => {
-                        if (xPlane.includes(face) && face != "back")
-                            setFace("bottom");
-                        else if (yPlane.indexOf(face) != 0)
-                            setFace(yPlane[yPlane.indexOf(face) - 1]);
-                        else setFace(yPlane[yPlane.length - 1]);
-                    }}
-                >
-                    V
-                </Button>
-            </div>
+            <a
+                className="button bg-blue hover:text-blue text-center"
+                href="/calendar"
+            >
+                Calendar
+            </a>
         </main>
     );
 }
