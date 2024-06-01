73ff3fce7da6f39131b18f4a43715b8f1033d8f1
DiabGarren <garrendiab@gmail.com>
2024-06-01 19:17:46 +0200

    Date

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,5 +1,7 @@
 export default function Calendar() {
-    return <main className="max-w-[900px] mx-auto my-[50px]">
+    let today = new Date();
+    return (<main className="max-w-[900px] mx-auto my-[50px]">
         <h1 className="text-center text-[34px]">Calendar</h1>
-    </main>;
+        <p>{today.getFullYear()}</p>
+    </main>);
 }
