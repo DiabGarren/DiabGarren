e7c8d0eb4c5a20a4906b8938a246f8db2c2e61e4
DiabGarren <garrendiab@gmail.com>
2024-06-01 19:14:08 +0200

    Added heading

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,3 +1,5 @@
 export default function Calendar() {
-    return <></>;
+    return <main className="max-w-[900px] mx-auto my-[50px]">
+        <h1 className="text-center text-[34px]">Calendar</h1>
+    </main>;
 }
