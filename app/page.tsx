0f03287adc9a57c54dcc1be03d7137cc164febc3
DiabGarren <garrendiab@gmail.com>
2025-07-24 12:50:14 +0200

    Moved App to: https://diab3dprinting.vercel.app/

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-    return (
-        <main className="w-[350px] mx-auto">
-            <a
-                className="text-white bg-blue border-[2px] border-blue block my-[10px] py-[5px] rounded text-center hover:bg-white hover:text-blue"
-                href="/3DPrinting"
-            >
-                3D Printing
-            </a>
-            <a
-                className="text-white bg-blue border-[2px] border-blue block my-[10px] py-[5px] rounded text-center hover:bg-white hover:text-blue"
-                href="/DiabMotors"
-            >
-                Diab Motors
-            </a>
-        </main>
-    );
+    const { push } = useRouter();
+
+    push("https://diab3dprinting.vercel.app/");
 }
--- components/DiabMotors/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="text-white p-[10px] text-[15px]">
-            Diab Motors is not a registered business
-            <br />
-            This site is intended for internal use only
-        </footer>
-    );
-}
